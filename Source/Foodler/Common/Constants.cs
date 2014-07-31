1324feb7a4d88e05a85fd6008876392141bf9dd6
Ivan Cherednychok <ravadog@gmail.com>
2014-07-31 22:13:01 +0300

    final of lite version v.1.0.0.0

--- Source/Foodler/Common/Constants.cs
+++ Source/Foodler/Common/Constants.cs
@@ -10,7 +10,7 @@ namespace Foodler.Common
         public const int MAX_PARTICIPANTS_AMOUNT = 3;
         public const int MAX_FOOD_AMOUNT = 5;
 
-        public const string URL_TO_FULL_VERSION = "http://www.windowsphone.com/s?appId=12017833-8a50-e011-854c-00237de2db9e";
+        public const string URL_TO_FULL_VERSION = "http://www.windowsphone.com/s?appId=367af9cc-9eea-4303-858f-cee01f8ff2ae";
 
         static Constants()
         {
