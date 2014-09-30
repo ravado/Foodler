1bfbb507e0e923027bb46edf29c66d113117d1a0
ravado <the.ravado@gmail.com>
2014-09-30 21:20:20 +0300

    made hotfix for participant duplication

--- Source/Foodler/Models/Participant.cs
+++ Source/Foodler/Models/Participant.cs
@@ -6,7 +6,7 @@ using Foodler.Common.Contracts;
 namespace Foodler.Models
 {
     [Table(Name = "Participants")]
-    public class Participant : BaseModel, IParticipant//, IEquatable<Participant>
+    public class Participant : BaseModel, IParticipant, IEquatable<Participant>
     {
         #region Fields
 
@@ -124,24 +124,24 @@ namespace Foodler.Models
             return String.Format("{0}, ate coeff: {1}", Name, ParticipantAteCoefficient);
         }
 
-        //public bool Equals(Participant other)
-        //{
-        //    if (other != null && other.Id == Id)
-        //        return true;
+        public bool Equals(Participant other)
+        {
+            if (other != null && other.Id == Id)
+                return true;
 
-        //    return false;
-        //}
+            return false;
+        }
 
-        //public override bool Equals(object obj)
-        //{
-        //    return Equals(obj as Participant);
-        //}
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Participant);
+        }
 
-        //public override int GetHashCode()
-        //{
-        //    var nameHash = (Name == null) ? 0 : Name.GetHashCode();
+        public override int GetHashCode()
+        {
+            var nameHash = (Name == null) ? 0 : Name.GetHashCode();
 
-        //    return Id.GetHashCode() + nameHash;
-        //}
+            return Id.GetHashCode() + nameHash;
+        }
     }
 }
